22c0e67f6292510f3078bbc7f9a21c60a3cb7364
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-06-08 06:10:47 +0900

    バリデーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PHONE_REGEX = /\A0\d{9,10}\z/
+
+  validates :name, allow_blank: true, length: { maximum: 20 }
+  validates :phone_number,
+            format: { with: VALID_PHONE_REGEX },
+            length: { in: 10..11 },
+            allow_blank: true,
+            uniqueness: true
 end
